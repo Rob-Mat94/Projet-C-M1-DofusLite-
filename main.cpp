70f6b4a57f695f41e31438f6757258f43d097f41
Rob-Mat94 <robitat@gmail.com>
2020-11-06 15:01:44 +0100

    poly

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 #include "includes/Guerrier.h"
 #include "includes/Obstacle.h"
 #include "includes/Position.h"
@@ -10,7 +10,16 @@ int main()
 {      
     ObjetRamassable o {100};
     Obstacle ob{Position(150,180)};
-    std::cout << " x : " << ob.getPosition().getPosX() << " / y : " << ob.getPosition().getPosY();  
-    
+   // std::cout << " x : " << ob.getPosition().getPosX() << " / y : " << ob.getPosition().getPosY();
+    std::vector<Element*> tab;  
+    tab.push_back(new Obstacle(Position(12,12)));
+    tab.push_back(new ObjetRamassable(50,Position(45,452)));
+
+    for(int i = 0; i < tab.size(); i++)
+    {
+        std::cout << tab[i]->getPosition().getPosX() << " / " << tab[i]->getPosition().getPosY() << std::endl; 
+        delete tab[i];
+    }
+
     return 0;
 }
