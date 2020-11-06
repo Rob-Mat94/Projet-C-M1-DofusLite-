6d4fc27288bac51d3c743cc671aaffff21f18707
Rob-Mat94 <robitat@gmail.com>
2020-11-06 11:49:10 +0100

    Position

---
+++ includes/Position.h
@@ -0,0 +1,22 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+class Position
+{
+    public:
+        Position(const int x = 0, const int y = 0);
+        int getPosX()const;
+        int getPosY()const;
+        void setPosX(const int x);
+        void setPosY(const int y);
+
+    private:
+        int _x;
+        int _y; 
+};
+
+#endif
+
+
+
+
---
+++ src/Position.cpp
@@ -0,0 +1,25 @@
+#include "../includes/Position.h"
+
+Position::Position(int x, int y):_x(x),_y(y){}
+
+int Position::getPosX()const
+{
+    return this->_x;
+}
+
+int Position::getPosY()const
+{
+    return this->_y;
+}
+
+void Position::setPosX(const int x)
+{
+    if(x >= 0)
+        this->_x = x;
+}
+
+void Position::setPosY(const int y)
+{
+    if(y >= 0)
+        this->_y = y;
+}
